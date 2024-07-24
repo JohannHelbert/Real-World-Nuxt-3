1d84cba547fa125f77a63953b7076b7ef1eecae7
passionate skye <johannhelbert@gmail.comgit config --global user.name JohannHelbert>
2024-07-24 14:05:25 +0200

    add client-side interactivity

---
+++ components/CategoryLink.vue
@@ -0,0 +1,33 @@
+<script setup lang='ts'>
+import type { Category } from '@/data/categories'
+
+const { category } = defineProps<{ category: Category}>()
+</script>
+
+<template>
+  <div class="category">
+    <a href="#">Category: {{ category.name }}</a>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+@import '/assets/styles/_colors';
+
+.category {
+    display: block;
+    margin: 15px 0px;
+}
+
+.category a {
+    color: $green-medium;
+    border: 1px solid $green-dark;
+    border-radius: 5px;
+    font-size: 12pt;
+    padding: 5px 10px;
+}
+
+.category a:hover {
+    color: $green-light;
+    border-color: $green-medium;
+}
+</style>
---
+++ components/PostCard.vue
@@ -0,0 +1,61 @@
+<script setup lang='ts'>
+import type { Post } from '@/data/posts'
+
+const { post } = defineProps<{ post: Post }>()
+
+const isHover = ref(false)
+</script>
+
+<template>
+  <div
+    class="card"
+    :class="{hover: isHover}"
+  >
+    <h2 class="title">
+      {{ post.title }}
+    </h2>
+    <CategoryLink :category="post.category" />
+    <RenderMarkdown :source="post.intro" />
+    <a href="#" class="more" @mouseenter="isHover = true" @mouseleave="isHover = false">
+      More ...
+    </a>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "~/assets/styles/_colors.scss";
+
+.card {
+  padding: 15px 15px 35px;
+  border: 1px solid $green-dark;
+  border-radius: 5px;
+  margin-bottom: 25px;
+  font-size: 12pt;
+  position: relative;
+}
+
+.card .title {
+  font-size: 20pt;
+}
+
+.card .more {
+    color: $green-medium;
+    font-size: 12pt;
+    padding: 5px 10px;
+    position: absolute;
+    right: 0px;
+    bottom: 0px;
+}
+
+.card.hover {
+    border-color: $green-medium;
+}
+
+.card.hover .title {
+    color: $green-light;
+}
+
+.card.hover .more {
+    color: $green-light;
+}
+</style>
